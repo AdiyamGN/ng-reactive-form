45ab21dcae5ea460bf855adceac7caeba35ed8d3
Adiyamseged Nerri <adiyamgn@outlook.com>
2017-07-08 18:03:46 -0400

    Added ReactiveFormsModule and neccessary functions from angular/forms.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  rForm: FormGroup;
+  post: any;                   // A property for our submitted form
+  description: string = '';
+  name: string = '';
+
+  constructor(fb: FormBuilder) {
+    this.rForm = fb.group({
+      'name': [null, Validators.required],
+      'description': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
+      'validate': ''
+    });
+  }
+
+  addPost(post) {
+    this.name = post.name;
+    this.description = post.description;
+  }
+
+
 }
